babf09231fec4a9b30ce2d04782f1722fc895d5a
Roberta Lima <robertataisaa@gmail.com>
2023-08-19 12:44:26 -0300

    Entity, services, e resources PRODUCT

---
+++ src/main/java/com/learnjava/project/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.learnjava.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.learnjava.project.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
